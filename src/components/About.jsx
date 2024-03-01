d34a484608dca8b41f6cf08de49dfde882fb6242
Yadnesh12 <yadneshbharambe123@gmail.com>
2024-03-01 18:03:27 +0530

    removed portfolio button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-        <p className='text-xl mt-20'>
+        <p className='text-xl lg:mt-20 sm:mt-10'>
           • Engineered and optimized frontend Components using React.JS,
           improving site performance by 30%. <br />
           • Utilized SonarQube to identify and rectify code quality issues and security vulnerabilities. <br />
@@ -20,7 +20,7 @@ const About = () => {
         </p>
         <br />
         <p className='text-xl'> 
-            More Details to be added
+          
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+// import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { FiDownloadCloud } from 'react-icons/fi';
-import { Link } from "react-scroll";
+// import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row"> 
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Frontend Developer</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a FrontEnd Developer
+          </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have 2.5 years of experience in building and designing applications at Infosys
             Limited, Specializing in React.JS, JavaScript and Oracle Jet.
@@ -18,7 +23,7 @@ const Home = () => {
           </p>
 
           <div>
-          <Link
+          {/* <Link
               to="portfolio"
               smooth
               duration={500}
@@ -28,7 +33,7 @@ const Home = () => {
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </Link>
+            </Link> */}
 
            <a href="/resume.pdf" download={true} className= "group" >
       <button className="bg-gradient-to-r  from-cyan-500 to-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 my-2 rounded inline-flex items-center lg:animate-pulse">
@@ -42,7 +47,7 @@ const Home = () => {
           </div>
         </div>
         <div>
-            {/* <img src={HeroImage} alt="my Profile" className="rounded-full mx-auto w-1/3 md:w-full" size={-10}/> */}
+            <img src={HeroImage} alt="my Profile" className="rounded-full mx-auto w-2/3 md:w-full" />
         </div>
       </div>
     </div>
